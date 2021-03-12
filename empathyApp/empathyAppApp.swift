7954785b7201760e902d5c70e7a4327c8c984004
mbrigham6 <70815341+mbrigham6@users.noreply.github.com>
2021-03-12 09:03:49 -0500

    Initial Commit

---
+++ empathyApp/empathyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  empathyAppApp.swift
+//  empathyApp
+//
+//  Created by Molly Brigham on 3/12/21.
+//
+
+import SwiftUI
+
+@main
+struct empathyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
