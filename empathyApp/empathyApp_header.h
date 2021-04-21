6a359239c2d03689ff099f8917fa5031dd8ede33
soccerbob97 <46753790+soccerbob97@users.noreply.github.com>
2021-04-21 15:19:23 -0400

    Add files via upload
    This is header file should be connected to objective c bridging header

---
+++ empathyApp/empathyApp_header.h
@@ -0,0 +1,18 @@
+//
+//  empathyApp_header.h
+//  empathyApp
+//
+//  Created by Harsha Karanth on 4/19/21.
+//
+
+#import "GVRPanoramaView.h"
+#import "GVRWidgetView.h"
+#import "GVRVideoView.h"
+
+#ifndef empathyApp_header_h
+#define empathyApp_header_h
+
+
+
+
+#endif /* empathyApp_header_h */
